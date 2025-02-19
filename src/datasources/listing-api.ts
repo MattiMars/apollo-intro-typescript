72fefc39d8c71a5b711c60df76825dbfdf947b8a
Mathias <mathias@mb-air.local>
2025-02-19 20:14:35 +0100

    done

---
+++ src/datasources/listing-api.ts
@@ -0,0 +1,25 @@
+import { RESTDataSource } from '@apollo/datasource-rest';
+import { Amenity, CreateListingInput, Listing } from '../types';
+
+export class ListingAPI extends RESTDataSource {
+  baseURL = 'https://rt-airlock-services-listing.herokuapp.com/';
+
+  getFeaturedListings() {
+    return this.get<Listing[]>('featured-listings');
+  }
+
+  getListing(listingId: string) {
+    return this.get<Listing>(`listings/${listingId}`);
+  }
+
+  getAmenities(listingId: string) {
+    console.log('Making a follow-up call for amenities with ', listingId);
+    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+  }
+
+  createListing(listing: CreateListingInput) {
+    return this.post<Listing>('listings', {
+      body: { listing },
+    });
+  }
+}
